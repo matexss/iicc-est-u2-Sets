5106f8b8f0b1f417fba976ef04e4f21a33b74a88
Mateo <mateomorejon993@gmail.com>
2025-07-01 08:47:11 -0500

    commit 1

---
+++ src/App.java
@@ -0,0 +1,36 @@
+import controller.Sets;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Sets sets = new Sets();
+
+        runHashSet(sets);
+        runLinkedHashSet(sets);
+        construirTreeSet(sets);
+        construirTreeSetConComparador(sets);  // Correcta invocación del método
+    }
+
+    public static void runHashSet(Sets sets) {
+        System.out.println("-----------HashSet----------");
+        System.out.println("ELEMENTOS DEL HASHSET NO SE GARANTIZA EL ORDEN");
+        sets.construirHashSet();  
+    }
+
+    public static void runLinkedHashSet(Sets sets) {
+        System.out.println("-----------LinkedHashSet----------");
+        System.out.println("ELEMENTOS DEL LINKEDHASHSET SE GARANTIZA EL ORDEN DE INSERCION");
+        sets.construirLinkedHashSet();  
+    }
+
+    public static void construirTreeSet(Sets sets) {
+        System.out.println("-----------TreeSet----------");
+        System.out.println("ELEMENTOS DEL TREESET (ORDEN ASCENDENTE)");
+        sets.construirTreeSet(); 
+    }
+
+    public static void construirTreeSetConComparador(Sets sets) {
+        System.out.println("-----------TreeSet Con Comparador----------");
+        System.out.println("ELEMENTOS DEL TREESET CON COMPARADOR (ORDEN POR LONGITUD Y ALFABÉTICO)");
+        sets.construirTreeSetConComparador(); 
+    }
+}
---
+++ src/controller/Sets.java
@@ -0,0 +1,88 @@
+package controller;
+
+import java.util.Set;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+
+public class Sets {
+
+    public Sets() {
+    }
+
+    public void construirHashSet() {
+        Set<String> palabras = new HashSet<>();
+        palabras.add("Laptop");
+        palabras.add("Manzana");
+        palabras.add("Pera");
+        palabras.add("Celular");
+        palabras.add("Laptop"); 
+        palabras.add("Pera"); 
+        palabras.add("Laptop"); 
+
+        for (String palabra : palabras) {
+            System.out.println(palabra);
+        }
+    }
+    
+    public void construirLinkedHashSet() {
+        Set<String> palabrasLinkeadas = new LinkedHashSet<>();
+         palabrasLinkeadas.add("Laptop");
+        palabrasLinkeadas.add("Manzana");
+        palabrasLinkeadas.add("Pera");
+        palabrasLinkeadas.add("Celular");
+        palabrasLinkeadas.add("Laptop"); 
+        palabrasLinkeadas.add("Pera"); 
+        palabrasLinkeadas.add("Laptop"); 
+
+        for (String palabra : palabrasLinkeadas) {
+            System.out.println(palabra);
+        }
+    }
+
+    public void construirTreeSet() {
+        Set<String> palabrasTreeSet = new TreeSet<>();
+         palabrasTreeSet.add("Laptop");
+        palabrasTreeSet.add("Manzana");
+        palabrasTreeSet.add("Pera");
+        palabrasTreeSet.add("Celular");
+        palabrasTreeSet.add("Laptop"); 
+        palabrasTreeSet.add("Pera"); 
+        palabrasTreeSet.add("Laptop"); 
+
+        for(String palabra : palabrasTreeSet){
+            System.out.println(palabra);
+        }
+    }
+
+    public void construirTreeSetConComparador() {
+        Comparator<String> miComparador = new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                int result = Integer.compare(s1.length(), s2.length());
+                // Si tienen la misma longitud, se comparan alfbabeticamente
+                if (result == 0) {
+                    return s1.compareTo(s2);
+                }
+                return result;
+            }
+        };
+
+        Set<String> palabrasTreeSet = new TreeSet<>(miComparador);
+        palabrasTreeSet.add("Laptop");
+        palabrasTreeSet.add("Manzana");
+        palabrasTreeSet.add("Pera");
+        palabrasTreeSet.add("Celular");
+        palabrasTreeSet.add("Laptop");
+        palabrasTreeSet.add("Pera");
+        palabrasTreeSet.add("Laptop");
+
+        for (String palabra : palabrasTreeSet) {
+            System.out.println(palabra);
+        }
+    }
+
+
+    
+}
